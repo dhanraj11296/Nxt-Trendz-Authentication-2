31230ccd5ba3ea945b5373fb5c6d643cd8325d97
dhanraj11296 <rvsanjayraj@gmail.com>
2024-07-08 12:32:39 +0000

    Solution

---
+++ src/components/Products/index.js
@@ -0,0 +1,24 @@
+// Write your JS code here
+import {Redirect} from 'react-router-dom'
+import Cookies from 'js-cookie'
+import Header from '../Header'
+import './index.css'
+const Products = () => {
+  const accessToken = Cookies.get('jwt_token')
+  if (accessToken === undefined) {
+    return <Redirect to="/login" />
+  }
+  return (
+    <>
+      <Header />
+      <div className="productsContainer">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+          alt="products"
+          className="productsImg"
+        />
+      </div>
+    </>
+  )
+}
+export default Products
